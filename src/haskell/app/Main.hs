c3d47a2b90946d188854d7ea7e1f65388dea3fea
LucZZ <lucas.weiss1999@web.de>
2024-12-09 15:47:16 +0100

    added Grid and print Grid to console

--- src/haskell/app/Main.hs
+++ src/haskell/app/Main.hs
@@ -1,4 +1,30 @@
 module Main where
 
+type Cell = Bool
+type Grid = [[Cell]]
+
+initGrid :: Grid
+initGrid = 
+      [ [False, False, False, False, False],
+        [False, False, False, False, False],
+        [False, False, True, False, False],
+        [False, False, False, False, False],
+        [False, False, False, False, False] ]
+
+drawGrid :: Grid -> IO ()
+drawGrid grid = mapM_ (putStrLn . map drawCell) grid
+  where
+    drawCell True  = 'X' 
+    drawCell False = '.'
+
+drawGridSimple :: Grid -> IO ()
+drawGridSimple [] = return ()
+drawGridSimple (row:rows) = do
+    putStrLn (map drawCell row) 
+    drawGrid rows               
+  where
+    drawCell True  = 'X'
+    drawCell False = '.'
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = drawGridSimple initGrid
