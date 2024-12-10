aca959927f3bec4549f1eb8fb23b2f68bbd50d6b
LucZZ <lucas.weiss1999@web.de>
2024-12-10 17:50:14 +0100

    Added countAliveNeighbours

--- src/haskell/app/Main.hs
+++ src/haskell/app/Main.hs
@@ -17,14 +17,22 @@ drawGrid grid = mapM_ (putStrLn . map drawCell) grid
     drawCell True  = 'X' 
     drawCell False = '.'
 
+-- Simple method with recursion
 drawGridSimple :: Grid -> IO ()
 drawGridSimple [] = return ()
 drawGridSimple (row:rows) = do
-    putStrLn (map drawCell row) 
-    drawGrid rows               
-  where
-    drawCell True  = 'X'
-    drawCell False = '.'
+    putStrLn (map drawCellSimple row) 
+    drawGridSimple rows               
+
+drawCellSimple :: Bool -> Char
+drawCellSimple True = 'X'
+drawCellSimple False = '.'
+
+cellInGrid :: Grid -> Int -> Int -> Bool
+cellInGrid grid x y = x >= 0 && y >= 0 && x < length grid && y < length (head grid)
+
+countAliveNeighbours :: Grid -> Int -> Int -> Int
+countAliveNeighbours grid x y = length  [() | i <- [x-1..x+1], j <- [y-1..y-1], (i /= x || j /= y), cellInGrid grid i j, grid !! i !! j]
 
 main :: IO ()
 main = drawGridSimple initGrid
