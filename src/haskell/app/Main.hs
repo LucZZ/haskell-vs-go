6c80c3a7354fddb222e37a5970b0eaf10dec10cf
LucZZ <lucas.weiss1999@web.de>
2024-12-14 10:27:17 +0100

    added nextGeneration

--- src/haskell/app/Main.hs
+++ src/haskell/app/Main.hs
@@ -32,7 +32,17 @@ cellInGrid :: Grid -> Int -> Int -> Bool
 cellInGrid grid x y = x >= 0 && y >= 0 && x < length grid && y < length (head grid)
 
 countAliveNeighbours :: Grid -> Int -> Int -> Int
-countAliveNeighbours grid x y = length  [() | i <- [x-1..x+1], j <- [y-1..y-1], (i /= x || j /= y), cellInGrid grid i j, grid !! i !! j]
+countAliveNeighbours grid x y = length  [() | i <- [x-1..x+1], j <- [y-1..y-1], i /= x || j /= y, cellInGrid grid i j, grid !! i !! j]
+
+nextStateForCell :: Grid -> Int -> Int -> Cell
+nextStateForCell grid x y = 
+    let neighbours = countAliveNeighbours grid x y
+        cell = grid !! x !! y
+    in neighbours == 3 || (cell && neighbours == 2)
+
+nextGeneration :: Grid -> Grid
+nextGeneration grid = [[nextStateForCell grid x y | y <- [0..length (head grid) - 1]]
+                                                  | x <- [0..length grid - 1]]
 
 main :: IO ()
 main = drawGridSimple initGrid
